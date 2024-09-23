0eed71c419175ba19a54384995bc1d00c4bb9ea2
soknem <povsoknem@gmail.com>
2024-09-23 13:49:37 +0700

    Auto commit

--- product-service/src/main/java/com/api_getway/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/api_getway/product_service/ProductServiceApplication.java
@@ -2,6 +2,8 @@ package com.api_getway.product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class ProductServiceApplication {
